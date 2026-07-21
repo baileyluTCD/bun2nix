7e2da328862983b820b61b3558f696fb5a9d162c
Luke Bailey <baileylu@tcd.ie>
2026-07-21 20:27:55 +0100

    `nix flake update` + default to `triplet` systems

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/triplet";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
